248cba27ea5920dcc279f501161e4213cbd5cbb0
Kwaku <kwakugyapong219@gmail.com>
2021-02-28 19:39:51 -0500

    Bake off

--- bake-off-challenge-frontend/index.js
+++ bake-off-challenge-frontend/index.js
@@ -1,2 +1,17 @@
 // your code here!
 console.log("🥧")
+
+fetch("http://localhost:3000/bakes")
+.then(response => response.json())
+.then(bakes => {
+     bakes.forEach(function(bake){
+         renderOneBake(bake)
+     })
+})
+const ul = document.querySelector("ul#bakes-container")
+function renderOneBake(bake) { 
+    let li = document.createElement("li")
+    ul.append(li)
+    li.innerHTML = `<li class="item" data-id="${bake.id}"> ${bake.name}</li>`
+
+}
